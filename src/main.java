af6f228b4a4fd82e6c42bbf86feb22b1287c563f
greggoryelton <55141543+greggoryelton@users.noreply.github.com>
2020-01-18 19:10:16 -0500

    import simpleNLG jar

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+import simplenlg.framework.*;
+import simplenlg.lexicon.*;
+import simplenlg.realiser.english.*;
+import simplenlg.phrasespec.*;
+import simplenlg.features.*;
 public class main {
     public static void main(String[] args) {
         System.out.println("test");
