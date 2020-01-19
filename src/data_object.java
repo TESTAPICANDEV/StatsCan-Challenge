6fae8d5551a8eb8c8c8cfee72af8bb883d2fbe3c
greggoryelton <55141543+greggoryelton@users.noreply.github.com>
2020-01-18 20:09:57 -0500

    add some code

--- src/data_object.java
+++ src/data_object.java
@@ -1,3 +1,33 @@
+
+
 public class data_object {
+    csv_reader csv;
+    String memberName = "";
+    int memberID = -1;
+    int parentID = -1;
+    int level = -1;
+    double[] data = {};
+
+    public data_object(String name, int memberID, int parentID, int level,double[] data ){
+        this.memberName = name;
+        this.memberID = memberID;
+        this.parentID = parentID;
+        this.level = level;
+        this.data = data;
+    }
+
+    public String getMemberName(){
+        return memberName;
+    }
+    public int getMemberID(){
+        return memberID;
+
+    }
+    public int getParentID(){
+        return parentID;
+    }
+    public double[] getData(){
+        return data;
+    }
 
 }
